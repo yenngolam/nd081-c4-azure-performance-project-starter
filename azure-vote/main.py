12880ac849e9a6ee24013141e814289a178a00b7
yenngolam <91681417+yenngolam@users.noreply.github.com>
2025-08-11 15:13:33 +0200

    Add files via upload

--- azure-vote/main.py
+++ azure-vote/main.py
@@ -9,21 +9,61 @@ from datetime import datetime
 
 # App Insights
 # TODO: Import required libraries for App Insights
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure.log_exporter import AzureEventHandler
+from opencensus.ext.azure import metrics_exporter
+from opencensus.stats import aggregation as aggregation_module
+from opencensus.stats import measure as measure_module
+from opencensus.stats import stats as stats_module
+from opencensus.stats import view as view_module
+from opencensus.tags import tag_map as tag_map_module
+from opencensus.trace import config_integration
+from opencensus.ext.azure.trace_exporter import AzureExporter
+from opencensus.trace.samplers import ProbabilitySampler
+from opencensus.trace.tracer import Tracer
+from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+# For metrics
+stats = stats_module.stats
+view_manager = stats.view_manager
 
+instrumentKey = "5605d795-0b33-4ce6-bfd7-7131e7c6b15d"
 # Logging
-logger = # TODO: Setup logger
-
+# TODO: Setup logger
+config_integration.trace_integrations(['logging'])
+config_integration.trace_integrations(['requests'])
+# Standard Logging
+logger = logging.getLogger(__name__)
+handler = AzureLogHandler(connection_string='InstrumentationKey=5605d795-0b33-4ce6-bfd7-7131e7c6b15d')
+handler.setFormatter(logging.Formatter('%(traceId)s %(spanId)s %(message)s'))
+logger.addHandler(handler)
+# Logging custom Events 
+logger.addHandler(AzureEventHandler(connection_string='InstrumentationKey=5605d795-0b33-4ce6-bfd7-7131e7c6b15d'))
+# Set the logging level
+logger.setLevel(logging.INFO)
 # Metrics
-exporter = # TODO: Setup exporter
-
+# TODO: Setup exporter
+exporter = metrics_exporter.new_metrics_exporter(
+enable_standard_metrics=True,
+connection_string='InstrumentationKey=5605d795-0b33-4ce6-bfd7-7131e7c6b15d')
+view_manager.register_exporter(exporter)
 # Tracing
-tracer = # TODO: Setup tracer
-
+# TODO: Setup tracer
+# Tracing
+tracer = Tracer(
+ exporter=AzureExporter(
+     connection_string='InstrumentationKey=5605d795-0b33-4ce6-bfd7-7131e7c6b15d'),
+ sampler=ProbabilitySampler(1.0),
+)
 app = Flask(__name__)
 
-# Requests
-middleware = # TODO: Setup flask middleware
 
+# Requests
+# TODO: Setup flask middleware
+middleware = FlaskMiddleware(
+ app,
+ exporter=AzureExporter(connection_string="InstrumentationKey=5605d795-0b33-4ce6-bfd7-7131e7c6b15d"),
+ sampler=ProbabilitySampler(rate=1.0)
+)
 # Load configurations from environment or config file
 app.config.from_pyfile('config_file.cfg')
 
@@ -60,9 +100,10 @@ def index():
 
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
-        # TODO: use tracer object to trace cat vote
+        tracer.span(name = "Get Cat Vote")
         vote2 = r.get(button2).decode('utf-8')
-        # TODO: use tracer object to trace dog vote
+        tracer.span(name="Get Dog Vote")
+
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
@@ -77,11 +118,12 @@ def index():
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
             # TODO: use logger object to log cat vote
+            logger.info('Cats Vote', extra=properties)
 
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
             # TODO: use logger object to log dog vote
-
+            logger.info('Dogs Vote', extra=properties)
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
         else:
@@ -99,6 +141,6 @@ def index():
 
 if __name__ == "__main__":
     # TODO: Use the statement below when running locally
-    app.run() 
+    # app.run() locally
     # TODO: Use the statement below before deployment to VMSS
-    # app.run(host='0.0.0.0', threaded=True, debug=True) # remote
+    app.run(host='0.0.0.0', threaded=True, debug=True) # remote
